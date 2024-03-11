04ac500391ae35e8e339e543921faac67b526989
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2024-03-11 21:33:13 +0500

    Add checks in tests for loop statements

--- tests/test_loop_statements.py
+++ tests/test_loop_statements.py
@@ -1,9 +1,35 @@
+import pytest
+import sys
 from plox.interpreter import Interpreter
 from plox.parser import Parser
 from plox.scanner import Scanner
 
 
-def test_while_loop():
+@pytest.fixture
+def capture_stdout(monkeypatch):
+    buffer = { "stdout": "", "write_calls": 0 }
+
+    def fake_write(s):
+        buffer["stdout"] += s
+        buffer["write_calls"] += 1
+
+    monkeypatch.setattr(sys.stdout, 'write', fake_write)
+    return buffer
+
+
+def run_code(code: str, capture_stdout) -> str:
+    scanner = Scanner(code)
+    tokens = scanner.scan_tokens()
+
+    parser = Parser(tokens)
+    statements = parser.parse()
+
+    interpreter = Interpreter()
+    interpreter.interpret(statements)
+
+
+
+def test_while_loop(capture_stdout):
     source = """
         var count = 0;
         while (count < 10) {
@@ -12,27 +38,16 @@ def test_while_loop():
         }    
     """
 
-    scanner = Scanner(source)
-    tokens = scanner.scan_tokens()
+    run_code(source, capture_stdout)
+    assert capture_stdout["stdout"] == "\n".join(map(str, list(range(10)))) + "\n"
 
-    parser = Parser(tokens)
-    statements = parser.parse()
 
-    interpreter = Interpreter()
-    interpreter.interpret(statements)
-
-def test_for_loop():
+def test_for_loop(capture_stdout):
     source = """
         for (var i = 0; i < 10; i = i + 1) {
             print i;
         }
     """
 
-    scanner = Scanner(source)
-    tokens = scanner.scan_tokens()
-
-    parser = Parser(tokens)
-    statements = parser.parse()
-
-    interpreter = Interpreter()
-    interpreter.interpret(statements)
+    run_code(source, capture_stdout)
+    assert capture_stdout["stdout"] == "\n".join(map(str, list(range(10)))) + "\n"
